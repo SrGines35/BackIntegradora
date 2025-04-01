b284d1b8d34e38709a749ab6dd93f300d71a4d0a
SrGines35 <ginesgm35@gmail.com>
2025-04-01 11:32:05 -0600

    ErrorCorregido

--- app/routes/productoRoutes.js
+++ app/routes/productoRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const productoController = require('../controllers/productoController');
 const upload = require('../../config/multerConfig');
-
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.get('/', productoController.obtenerProductos);
 router.get('/:id', productoController.obtenerProductoPorId);
@@ -12,7 +12,11 @@ router.get('/nombre/:nombre', productoController.obtenerProductosPorNombre);
 router.get('/favoritos/favorito=true', productoController.obtenerProductosFavoritos);
 
 // Crear un producto con subida de imágenes
-router.post('/', upload.array('imagenes', 3), productoController.crearProducto);
+// router.post('/', upload.array('imagenes', 3), productoController.crearProducto);
+router.post('/', authMiddleware, // Middleware de autenticación
+upload.array('imagenes', 3), // Máximo 3 imágenes
+    productoController.crearProducto
+    );
 
 // Actualizar un producto con subida de imágenes
 router.put('/:id', upload.array('imagenes', 3), productoController.actualizarProducto);
